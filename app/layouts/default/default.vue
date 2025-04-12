f5d81435e2d043aa33c70398ad7127f94c16ae44
i.lyamcev <Brossend23350@mail.ru>
2025-04-13 03:52:31 +0600

    feat: адаптивная тема

---
+++ app/layouts/default/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <header :class="['pt-[14px] pb-[14px] border-b', headerClass, borderClass]">
+    <UContainer class="flex flex-row justify-center items-center gap-1">
+      <UIcon class="size-[40px]" name="hugeicons:electric-home-02" />
+      <span>DomConnect</span>
+    </UContainer>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer />
+</template>
+
+<script lang="ts" setup>
+const colorMode = useColorMode();
+
+const headerClass = computed(() => {
+  return colorMode.value === "dark"
+    ? "bg-slate-700 text-white"
+    : "bg-indigo-600 text-white";
+});
+
+const borderClass = computed(() => {
+  return colorMode.value === "dark" ? "border-slate-600" : "border-indigo-700";
+});
+</script>
+
+<style scoped></style>
