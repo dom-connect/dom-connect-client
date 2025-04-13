e79d1c6f641d4fa59b51b58c9546b161e59814aa
i.lyamcev <brossend23350@mail.ru>
2025-04-14 03:35:13 +0600

    feat: главная страница (70%)

--- app/layouts/default/default.vue
+++ app/layouts/default/default.vue
@@ -1,117 +1,126 @@
 <template>
   <header
-    class="pt-[14px] pb-[14px] border-b border-neutral-700 flex flex-row bg-neutral-800 text-blue-400"
+    :class="viewport.isLessThan('desktop') ? 'fixed w-full z-10 top-0' : ''"
+    class="pt-[14px] pb-[14px] border-b border-neutral-700 bg-neutral-800 text-blue-400"
   >
-    <UContainer
-      :class="
-        !viewport.isLessThan('desktop') ? 'justify-center' : 'justify-start'
-      "
-      class="flex flex-row items-center gap-2"
-    >
-      <UIcon class="size-[40px]" name="hugeicons:electric-home-02" />
-      <span>DomConnect</span>
-    </UContainer>
-    <UContainer
-      v-if="!viewport.isLessThan('desktop')"
-      class="flex flex-row justify-center items-center gap-6"
-    >
-      <ULink
-        v-for="(item, number) in links"
-        :key="number"
-        :to="`/${item.link}`"
-        as="button"
-        >{{ item.name }}
-      </ULink>
-    </UContainer>
-    <UContainer
-      v-if="!viewport.isLessThan('desktop')"
-      class="flex flex-row justify-center items-center gap-6"
-    >
-      <UButton
-        class="cursor-pointer"
-        color="primary"
-        icon="hugeicons:moon-02"
-        size="md"
-        variant="subtle"
-        @click="toggleTheme"
-      />
-      <UButton
-        class="cursor-pointer"
-        color="primary"
-        icon="hugeicons:logout-02"
-        size="md"
-        variant="subtle"
-        @click="
-          () => {
-            useCookie('access_token').value = '';
-            navigateTo('/auth', { replace: true });
-          }
+    <UContainer class="flex flex-row">
+      <div
+        :class="
+          !viewport.isLessThan('desktop') ? 'justify-center' : 'justify-start'
         "
-      />
-    </UContainer>
-    <UContainer
-      v-if="viewport.isLessThan('desktop')"
-      class="flex flex-row justify-end items-center gap-6"
-    >
-      <USlideover v-model:open="open">
+        class="flex w-fit flex-row items-center gap-2"
+      >
+        <UIcon class="size-[40px]" name="hugeicons:electric-home-02" />
+        <span>DomConnect</span>
+      </div>
+      <div
+        v-if="!viewport.isLessThan('desktop')"
+        class="flex flex-row w-full justify-center items-center gap-6"
+      >
+        <ULink
+          v-for="(item, number) in links"
+          :key="number"
+          :to="`/${item.link}`"
+          as="button"
+          >{{ item.name }}
+        </ULink>
+      </div>
+      <div
+        v-if="!viewport.isLessThan('desktop')"
+        class="flex w-fit flex-row justify-center items-center gap-6"
+      >
         <UButton
           class="cursor-pointer"
           color="primary"
-          icon="hugeicons:menu-01"
+          icon="hugeicons:moon-02"
           size="md"
           variant="subtle"
+          @click="toggleTheme"
         />
-        <template #content>
-          <UContainer
-            class="flex flex-row h-[72px] justify-between items-center pt-[16px] pb-[16px] border-b border-neutral-700"
-          >
-            <span>Меню</span>
-            <UButton
-              class="cursor-pointer"
-              color="primary"
-              icon="hugeicons:cancel-01"
-              size="md"
-              variant="subtle"
-              @click="open = false"
-            />
-          </UContainer>
-          <UContainer class="flex flex-col h-[100%] gap-2 pt-[16px] pb-[16px]">
-            <ULink
-              v-for="(item, number) in links"
-              :key="number"
-              :to="`/${item.link}`"
-              as="button"
-              >{{ item.name }}
-            </ULink>
-            <UButton
-              class="cursor-pointer mt-auto"
-              color="primary"
-              icon="hugeicons:moon-02"
-              size="md"
-              variant="subtle"
-              @click="toggleTheme"
-              >Сменить тему
-            </UButton>
-            <UButton
-              class="cursor-pointer"
-              color="primary"
-              icon="hugeicons:logout-02"
-              size="md"
-              variant="subtle"
-              @click="
-                () => {
-                  useCookie('access_token').value = '';
-                  navigateTo('/auth', { replace: true });
-                }
-              "
-              >Выйти
-            </UButton>
-          </UContainer>
-        </template>
-      </USlideover>
+        <UButton
+          class="cursor-pointer"
+          color="primary"
+          icon="hugeicons:logout-02"
+          size="md"
+          variant="subtle"
+          @click="
+            () => {
+              useCookie('access_token').value = '';
+              navigateTo('/auth', { replace: true });
+            }
+          "
+        />
+      </div>
+      <UContainer
+        v-if="viewport.isLessThan('desktop')"
+        class="flex flex-row justify-end items-center gap-6"
+      >
+        <USlideover v-model:open="open">
+          <UButton
+            class="cursor-pointer"
+            color="primary"
+            icon="hugeicons:menu-01"
+            size="md"
+            variant="subtle"
+          />
+          <template #content>
+            <UContainer
+              class="flex flex-row h-[72px] justify-between items-center pt-[16px] pb-[16px] border-b border-neutral-700"
+            >
+              <span>Меню</span>
+              <UButton
+                class="cursor-pointer"
+                color="primary"
+                icon="hugeicons:cancel-01"
+                size="md"
+                variant="subtle"
+                @click="open = false"
+              />
+            </UContainer>
+            <UContainer
+              class="flex flex-col h-[100%] gap-2 pt-[16px] pb-[16px]"
+            >
+              <ULink
+                v-for="(item, number) in links"
+                :key="number"
+                :to="`/${item.link}`"
+                as="button"
+                >{{ item.name }}
+              </ULink>
+              <UButton
+                class="cursor-pointer mt-auto"
+                color="primary"
+                icon="hugeicons:moon-02"
+                size="md"
+                variant="subtle"
+                @click="toggleTheme"
+                >Сменить тему
+              </UButton>
+              <UButton
+                class="cursor-pointer"
+                color="primary"
+                icon="hugeicons:logout-02"
+                size="md"
+                variant="subtle"
+                @click="
+                  () => {
+                    useCookie('access_token').value = '';
+                    navigateTo('/auth', { replace: true });
+                  }
+                "
+                >Выйти
+              </UButton>
+            </UContainer>
+          </template>
+        </USlideover>
+      </UContainer>
     </UContainer>
   </header>
-  <main>
+  <main
+    style="height: calc(100% - 69px)"
+    class="pb-[32px]"
+    :class="viewport.isLessThan('desktop') ? 'pt-[69px]' : ''"
+  >
     <slot />
   </main>
   <footer />
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,167 @@
 <template>
-  <div class="w-full flex flex-row p-10" />
+  <UContainer
+    :class="viewport.isLessThan('desktop') ? 'pt-[16px]' : 'pt-[32px]'"
+    class="flex flex-row h-full justify-between gap-[32px]"
+  >
+    <div
+      :class="
+        viewport.isLessThan('desktop')
+          ? 'flex-col w-full gap-[16px] pb-[16px]'
+          : 'flex-row gap-[32px] w-[70%]'
+      "
+      class="flex"
+    >
+      <div
+        class="flex flex-col h-fit m-0 border border-gray-200 p-[20px] w-full rounded-[12px] border-neutral-700 bg-neutral-800"
+      >
+        <div class="flex flex-row justify-between pb-[24px]">
+          <span class="font-bold">ЕПД</span>
+          <UBadge class="text-white" color="error">Задолженность</UBadge>
+        </div>
+        <span class="pb-[20px] text-2xl">-1337.228 ₽</span>
+        <span class="pb-[30px] text-sm text-neutral-400"
+          >Обновлено 07.04.2025</span
+        >
+        <UButton class="mb-[20px] justify-center" variant="subtle"
+          >Оплатить</UButton
+        >
+        <UCollapsible class="flex flex-col gap-2">
+          <UButton
+            variant="subtle"
+            trailing-icon="i-lucide-chevron-down"
+            class="justify-center"
+          />
+
+          <template #content>
+            <div
+              class="flex flex-row justify-between pt-[20px] pb-[20px] items-center border-b border-neutral-700"
+            >
+              <div class="flex flex-col">
+                <span class="font-bold">ГВС</span>
+                <div class="flex flex-row items-center justify-between">
+                  <span>145 м2</span>
+                </div>
+              </div>
+              <UButton variant="subtle">Оплатить 610.34 ₽</UButton>
+            </div>
+            <div
+              class="flex flex-row justify-between pt-[20px] pb-[20px] items-center border-b border-neutral-700"
+            >
+              <div class="flex flex-col">
+                <span class="font-bold">ГВС</span>
+                <div class="flex flex-row items-center justify-between">
+                  <span>145 м2</span>
+                </div>
+              </div>
+              <UButton variant="subtle">Оплатить 610.34 ₽</UButton>
+            </div>
+            <div
+              class="flex flex-row justify-between pt-[20px] pb-[20px] items-center border-b border-neutral-700"
+            >
+              <div class="flex flex-col">
+                <span class="font-bold">ГВС</span>
+                <div class="flex flex-row items-center justify-between">
+                  <span>145 м2</span>
+                </div>
+              </div>
+              <UButton variant="subtle">Оплатить 610.34 ₽</UButton>
+            </div>
+            <div class="flex flex-row justify-between pt-[20px] items-center">
+              <div class="flex flex-col">
+                <span class="font-bold">ГВС</span>
+                <div class="flex flex-row items-center justify-between">
+                  <span>145 м2</span>
+                </div>
+              </div>
+              <UButton variant="subtle">Оплатить 610.34 ₽</UButton>
+            </div>
+          </template>
+        </UCollapsible>
+      </div>
+      <div
+        class="flex flex-col h-fit m-0 border border-gray-200 p-[20px] w-full rounded-[12px] border-neutral-700 bg-neutral-800"
+      >
+        <div class="flex flex-row justify-between pb-[24px]">
+          <span class="font-bold">Капитальный ремонт</span>
+          <UBadge v-if="false" class="text-white" color="error"
+            >Задолженность</UBadge
+          >
+        </div>
+        <span class="pb-[20px] text-2xl">-1337.228 ₽</span>
+        <span class="pb-[30px] text-sm text-neutral-400"
+          >Обновлено 07.04.2025</span
+        >
+        <UButton class="mb-[20px] justify-center" variant="subtle"
+          >Оплатить</UButton
+        >
+        <UCollapsible class="flex flex-col gap-2">
+          <UButton
+            variant="subtle"
+            trailing-icon="i-lucide-chevron-down"
+            class="justify-center"
+          />
+
+          <template #content>
+            <div
+              class="flex flex-row justify-between pt-[20px] pb-[20px] items-center border-b border-neutral-700"
+            >
+              <div class="flex flex-col">
+                <span class="font-bold">ГВС</span>
+                <div class="flex flex-row items-center justify-between">
+                  <span>145 м2</span>
+                </div>
+              </div>
+              <UButton variant="subtle">Оплатить 610.34 ₽</UButton>
+            </div>
+            <div
+              class="flex flex-row justify-between pt-[20px] pb-[20px] items-center border-b border-neutral-700"
+            >
+              <div class="flex flex-col">
+                <span class="font-bold">ГВС</span>
+                <div class="flex flex-row items-center justify-between">
+                  <span>145 м2</span>
+                </div>
+              </div>
+              <UButton variant="subtle">Оплатить 610.34 ₽</UButton>
+            </div>
+            <div
+              class="flex flex-row justify-between pt-[20px] pb-[20px] items-center border-b border-neutral-700"
+            >
+              <div class="flex flex-col">
+                <span class="font-bold">ГВС</span>
+                <div class="flex flex-row items-center justify-between">
+                  <span>145 м2</span>
+                </div>
+              </div>
+              <UButton variant="subtle">Оплатить 610.34 ₽</UButton>
+            </div>
+            <div class="flex flex-row justify-between pt-[20px] items-center">
+              <div class="flex flex-col">
+                <span class="font-bold">ГВС</span>
+                <div class="flex flex-row items-center justify-between">
+                  <span>145 м2</span>
+                </div>
+              </div>
+              <UButton variant="subtle">Оплатить 610.34 ₽</UButton>
+            </div>
+          </template>
+        </UCollapsible>
+      </div>
+      <div class="min-h-[16px] mt-[-16px]" />
+    </div>
+    <div
+      v-if="!viewport.isLessThan('desktop')"
+      class="flex flex-col h-full m-0 border border-gray-200 p-[20px] w-[30%] rounded-[12px] border-neutral-700 bg-neutral-800"
+    >
+      <span class="font-bold">Новости</span>
+    </div>
+  </UContainer>
 </template>
 
 <script lang="ts" setup>
 useHead({
   title: "DomConnect – Главная",
 });
+
+const viewport = useViewport();
 </script>
